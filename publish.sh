a96f36f2acd4f277fe8494635765b5f02aefc97a
Igor Babuschkin <igor@babuschk.in>
2015-06-09 10:39:51 +0200

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 ghc site
 ./site build
-ghp-import -n ./_site && git push -fq https://${GH_TOKEN}@github.com/$(TRAVIS_REPO_SLUG).git gh-pages > /dev/null
+ghp-import -n ./_site && git push -fq https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
 
