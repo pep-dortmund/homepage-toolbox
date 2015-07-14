4b4fbc52e474fa4666089417ef3749202b89ed30
Igor Babuschkin <igor@babuschk.in>
2015-07-14 22:14:51 +0200

    Travis: Also publish to .org domain

--- publish.sh
+++ publish.sh
@@ -2,3 +2,6 @@
 
 ghp-import -n ./_site && git push -fq https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
 
+echo 'toolbox.pep-dortmund.org' > ./_site/CNAME
+ghp-import -n ./_site && git push -fq https://$GH_TOKEN@github.com/pep-dortmund/toolbox.pep-dortmund.org gh-pages > /dev/null
+
