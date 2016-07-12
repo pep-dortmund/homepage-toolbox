02fa431daebbf1a4725d9fe4c015c64b4ec5c134
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-07-12 22:28:43 +0200

    Change deploy system to use deploy key

--- publish.sh
+++ publish.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 echo 'toolbox.pep-dortmund.de'  > ./_site/CNAME
-ghp-import -n ./_site && git push -fq https://$GH_TOKEN@github.com/pep-dortmund/toolbox.pep-dortmund.de  gh-pages > /dev/null
-
-echo 'toolbox.pep-dortmund.org' > ./_site/CNAME
-ghp-import -n ./_site && git push -fq https://$GH_TOKEN@github.com/pep-dortmund/toolbox.pep-dortmund.org gh-pages > /dev/null
+ghp-import -n ./_site && git push -fq git@github.com:pep-dortmund/toolbox.pep-dortmund.de  gh-pages > /dev/null
