441aff3ddf525dd65c4d58a9661557f0d1120e1e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-08-30 15:36:09 +0200

    Deploy to uberspace

---
+++ .deploy_site.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# clear and re-create the out directory
+if [[ $TRAVIS_BRANCH == 'master' ]] && [[ $TRAVIS_PULL_REQUEST = false ]]
+then
+	# add remote ssh-key to key-storage
+	# first add remote host to known hosts
+	ssh-keyscan -t rsa $DEPLOY_HOST 2> /dev/null | sort -u - ~/.ssh/known_hosts -o ~/.ssh/known_hosts
+	# decrypt private shh key
+	openssl aes-256-cbc -K $encrypted_eb190b61ccca_key -iv $encrypted_eb190b61ccca_iv -in deploy_key.enc -out deploy_key -d
+
+
+	# start ssh-agent and add the key
+	eval "$(ssh-agent -s)"
+	chmod 600 deploy_key
+	ssh-add deploy_key
+
+	rm -rf ~/out || exit 0;
+	# compile the website
+	bundle exec jekyll build -d ~/out --config _config.yml
+	# upload site
+	rsync -rq --delete --exclude=".*" ~/out/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+else
+	echo "NOT ON MASTER BRANCH, WILL NOT DEPLOY SITE"
+fi
