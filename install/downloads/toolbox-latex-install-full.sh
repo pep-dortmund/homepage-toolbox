0a8383af674a7437e76158e21d59aad01bbc1ddb
Fabian Schierok <fabian.schierok@udo.edu>
2021-07-19 12:23:59 +0200

    mirrored changes from mini

--- install/downloads/toolbox-latex-install-full.sh
+++ install/downloads/toolbox-latex-install-full.sh
@@ -13,14 +13,14 @@ curl -L http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz | tar
 echo "selected_scheme scheme-full" > toolbox.profile
 
 # install LaTeX
-TEXLIVE_INSTALL_PREFIX=~/.local/texlive ./install-tl-*/install-tl -profile toolbox.profile
+TEXLIVE_INSTALL_PREFIX=$HOME/.local/texlive ./install-tl-*/install-tl -profile toolbox.profile
 
 # add to PATH
 echo 'export PATH="$HOME/.local/texlive/2021/bin/x86_64-linux:$PATH"' >> $HOME/.bashrc
 
 
 # set options
-cd $HOME/.local/.texlive/2021/bin/x86_64-linux
+cd $HOME/.local/texlive/2021/bin/x86_64-linux
 
 ./tlmgr option autobackup -- -1
 ./tlmgr option repository http://mirror.ctan.org/systems/texlive/tlnet
